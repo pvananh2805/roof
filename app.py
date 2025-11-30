32bcb16db8be9a2b6c15b4ae5e6bce4c69d9db2b
pvananh2805 <pvananh2805@gmail.com>
2025-11-30 23:37:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def index():
         ]
     }
     return render_template("index.html", profile=profile)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render sẽ cấp PORT qua biến môi trường
+    app.run(host="0.0.0.0", port=port, debug=True)
